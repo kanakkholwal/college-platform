312138b2bfd2ca2bdea1acdf58c867df4f872a8e
Kanak <kanakkholwal@gmail.com>
2024-05-17 13:10:33 +0530

    chore: Refactor time-table creation page and fix UI issues

--- app/(dashboard)/cr/time-tables/create/page.tsx
+++ app/(dashboard)/cr/time-tables/create/page.tsx
@@ -1,10 +1,18 @@
-import { TimeTable, sampleTimetableData } from "@/components/custom/time-table";
+import { TimeTable } from "@/components/custom/time-table";
+import { StoreInitializer } from "@/components/custom/time-table-editor";
+import { useTimetableStore } from "@/components/custom/time-table-store";
 import { getSession } from "src/lib/auth";
+import { createTimeTable } from "src/lib/time-table/actions";
 import { sessionType } from "src/types/session";
-import {createTimeTable} from "src/lib/time-table/actions";
+
 
 export default async function Dashboard() {
     const session = await getSession() as sessionType;
 
-    return (<TimeTable timetableData={sampleTimetableData} mode="edit" saveTimetable={createTimeTable}/>);
+
+
+    return (<>
+        <StoreInitializer timetableData={useTimetableStore.getInitialState().timetableData} isEditing={false} />
+        <TimeTable timetableData={useTimetableStore.getInitialState().timetableData} mode="edit" saveTimetable={createTimeTable} />
+    </>)
 }
